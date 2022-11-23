3b02540819c6f45b27243dbb44ac88f74c16e290
Pablo Jesus <91506505+PabloJesusdaSilva@users.noreply.github.com>
2022-11-23 17:02:15 -0300

    Adicionando animações com javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 1500,
+    reset: false
+})
+
+ScrollReveal().reveal('.header-pmt', {delay:400})
+ScrollReveal().reveal('.header-name', {delay:400})
+ScrollReveal().reveal('.banner-img', {delay: 400})
+ScrollReveal().reveal('.title-img', {delay: 800}) 
+ScrollReveal().reveal('.destaques', {delay: 800})
+ScrollReveal().reveal('.brindes', {delay: 800})
+ScrollReveal().reveal('.description', {delay: 800})
+ScrollReveal().reveal('.card-product', {delay: 800})
