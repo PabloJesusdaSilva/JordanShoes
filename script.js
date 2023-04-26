185916d824283dd1a475dfdb98d663e828c522e0
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-04-26 15:38:50 -0300

    Adicionando responsividade

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ const sr = ScrollReveal({
     distance: '30px',
     duration: 1500,
     reset: false
-})
+});
 
-ScrollReveal().reveal('.header-pmt', {delay:400})
-ScrollReveal().reveal('.header-name', {delay:400})
-ScrollReveal().reveal('.banner-img', {delay: 400})
-ScrollReveal().reveal('.title-img', {delay: 800}) 
-ScrollReveal().reveal('.destaques', {delay: 800})
-ScrollReveal().reveal('.brindes', {delay: 800})
-ScrollReveal().reveal('.description', {delay: 800})
-ScrollReveal().reveal('.card-product', {delay: 800})
+ScrollReveal().reveal('.promotion-header', {delay:400});
+ScrollReveal().reveal('.title-header', {delay:400});
+ScrollReveal().reveal('.banner', {delay: 400});
+ScrollReveal().reveal('.text-banner', {delay: 800});
+ScrollReveal().reveal('.emphasis-text', {delay: 800});
+ScrollReveal().reveal('.cards', {delay: 800});
